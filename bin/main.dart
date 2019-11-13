1929228a0985ec78868b7eb222d25d5a4402d764
onatcipli <onatcipli@outlook.com>
2019-11-13 23:52:40 +0300

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_generate/flutter_generate.dart' as flutter_generate;
+
+main(List<String> arguments) {
+  print('Hello world: ${flutter_generate.calculate()}!');
+}
---
+++ test/flutter_generate_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_generate/flutter_generate.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
